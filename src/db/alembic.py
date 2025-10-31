7eac57532d2c755294ef327f772d49fe3703fb34
Robert Antonio <robert@antonio.cz>
2025-10-31 22:09:08 +0100

    MT-18: add sample table to test alembic migrations

--- src/db/alembic.py
+++ src/db/alembic.py
@@ -2,3 +2,4 @@ from .engine import sa_metadata  # noqa
 
 # Import all tables from .tables package here
 # add # noqa at the end of each import, so ruff won't remove these unused imports
+from .tables.test import test_table  # noqa
