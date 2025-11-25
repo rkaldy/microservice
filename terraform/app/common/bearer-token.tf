c5f3e675a23236cc7c8272929193a6e19b0ec801
Robert Antonio <robert@antonio.cz>
2025-11-25 13:39:15 +0100

    MT-44: generate bearer token in terraform

---
+++ terraform/app/common/bearer-token.tf
@@ -0,0 +1,16 @@
+resource "random_password" "bearer_token" {
+  length  = 32
+  special = false
+}
+
+resource "google_secret_manager_secret" "bearer_token" {
+  secret_id = "${var.k8s_namespace}-${var.env}-bearer-token"
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "bearer_token" {
+  secret      = google_secret_manager_secret.bearer_token.id
+  secret_data = random_password.bearer_token.result
+}
--- terraform/app/common/providers.tf
+++ terraform/app/common/providers.tf
@@ -8,5 +8,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.38.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
 }
